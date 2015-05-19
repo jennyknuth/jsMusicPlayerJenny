c29561234ae84bb218704d641c917a6481aed9f4
Jenny Knuth <inkboco@gmail.com>
2015-05-18 19:35:46 -0600

    basic interaction working, need to generalize to all the buttons

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-alert("hello!)
+var button = document.getElementsByClassName('fa');
+var song =document.getElementsByTagName('audio');
+var header = document.getElementsByTagName('header');
+var songName = document.getElementsByClassName('song');
 
+button[0].addEventListener('click', function(e){
+
+  console.log(e);
+  button[0].classList.toggle('fa-play');
+  button[0].classList.toggle('fa-stop');
+  if(button[0].classList.contains('fa-stop')){
+    song[0].play();
+    header[0].innerHTML = 'Now Playing: ' + songName[0].innerHTML;
+  }
+  else {
+    song[0].pause();
+    header[0].innerHTML = 'Select a song!';
+  }
+
+});
