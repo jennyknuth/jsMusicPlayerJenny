d3e3acca4a292c5cc1c1c22c0007c0d5a14565cf
Jenny Knuth <inkboco@gmail.com>
2015-05-18 21:41:42 -0600

    added same js functionality to all the songs, added a reset file, ready to start CSS

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var button = document.getElementsByClassName('fa');
 var song =document.getElementsByTagName('audio');
 var header = document.getElementsByTagName('header');
-var songName = document.getElementsByClassName('song');
+var songName = document.getElementsByTagName('cite');
 
-button[0].addEventListener('click', function(e){
+button[0].addEventListener('click', function(event){
 
-  console.log(e);
-  button[0].classList.toggle('fa-play');
+  console.dir(event);
+  console.log(event.target);
+  console.dir(event.target);
+  console.log(button);
+  event.target.classList.toggle('fa-play');
   button[0].classList.toggle('fa-stop');
   if(button[0].classList.contains('fa-stop')){
     song[0].play();
@@ -14,7 +17,65 @@ button[0].addEventListener('click', function(e){
   }
   else {
     song[0].pause();
-    header[0].innerHTML = 'Select a song!';
+    header[0].innerHTML = 'Select a song';
   }
 
 });
+
+button[1].addEventListener('click', function(event){
+
+  console.dir(event);
+  console.log(event.target);
+  console.dir(event.target);
+  console.log(button);
+  event.target.classList.toggle('fa-play');
+  button[1].classList.toggle('fa-stop');
+  if(button[1].classList.contains('fa-stop')){
+    song[1].play();
+    header[0].innerHTML = 'Now Playing: ' + songName[1].innerHTML;
+  }
+  else {
+    song[1].pause();
+    header[0].innerHTML = 'Select a song';
+  }
+
+});
+
+button[2].addEventListener('click', function(event){
+
+  console.dir(event);
+  console.log(event.target);
+  console.dir(event.target);
+  console.log(button);
+  event.target.classList.toggle('fa-play');
+  button[2].classList.toggle('fa-stop');
+  if(button[2].classList.contains('fa-stop')){
+    song[2].play();
+    header[0].innerHTML = 'Now Playing: ' + songName[2].innerHTML;
+  }
+  else {
+    song[2].pause();
+    header[0].innerHTML = 'Select a song';
+  }
+
+});
+
+button[3].addEventListener('click', function(event){
+
+  console.dir(event);
+  console.log(event.target);
+  console.dir(event.target);
+  console.log(button);
+  event.target.classList.toggle('fa-play');
+  button[3].classList.toggle('fa-stop');
+  if(button[3].classList.contains('fa-stop')){
+    song[3].play();
+    header[0].innerHTML = 'Now Playing: ' + songName[3].innerHTML;
+  }
+  else {
+    song[3].pause();
+    header[0].innerHTML = 'Select a song';
+  }
+
+});
+
