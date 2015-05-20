d83d54fc9d41bc002b9d00905831cd06fd77b34f
Jenny Knuth <inkboco@gmail.com>
2015-05-20 13:07:27 -0600

    got it to reset before playing again, finally, and no thanks to forEach

--- app2.js
+++ app2.js
@@ -1,10 +1,15 @@
 var button = document.getElementsByClassName('fa');
+var audio = document.getElementsByTagName('audio');
 
 for (i = 0; i < button.length; i++){
-  button[i].addEventListener('click', function(event){
+ button[i].addEventListener('click', function(event){
     var song = event.target.parentNode.childNodes[5];
     var header = event.target.parentNode.parentNode.childNodes[1];
     var title = event.target.parentNode.childNodes[3].childNodes[0];
+    for (j = 0; j < button.length; j++){
+      button[j].className = 'fa fa-play';
+      audio[j].pause();
+    };
     event.target.classList.toggle('fa-play');
     event.target.classList.toggle('fa-stop');
     if (event.target.classList.contains('fa-stop')){
